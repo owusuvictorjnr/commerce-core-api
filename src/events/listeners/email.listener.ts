f6d6b97cd81250c9b147be783529bb6e9e296bc4
owusuvictorjnr <owusuvictor80@gmail.com>
2026-03-12 12:35:27 +0000

    fix: wrap email listener registration in a function for better modularity

--- src/events/listeners/email.listener.ts
+++ src/events/listeners/email.listener.ts
@@ -1,6 +1,8 @@
 import { eventBus } from "../event-bus.js";
 import { EVENTS } from "../event.types.js";
 
-eventBus.on(EVENTS.USER_CREATED, async (user) => {
+export const registerEmailListener = () => {
+    eventBus.on(EVENTS.USER_CREATED, async (user) => {
     console.log("Email sent to", user.email);
-})
+    });
+};
