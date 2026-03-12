07dd9ad3da4c717a90d5263ba169c3495d475e2d
owusuvictorjnr <owusuvictor80@gmail.com>
2026-03-12 12:59:57 +0000

    fix: update prisma client import to use getPrismaClient function for consistency

--- src/modules/orders/order.service.ts
+++ src/modules/orders/order.service.ts
@@ -1,11 +1,12 @@
 import { hookManager } from "../../hooks/hooks-manager.js";
-import prisma from "../../database/prisma-client.js";
+import getPrismaClient from "../../database/prisma-client.js";
 import { Prisma } from "@prisma/client";
 
 export const createdOrder = async (
   tenantId: string,
   data: Omit<Prisma.OrderCreateInput, "tenantId">,
 ) => {
+  const prisma = getPrismaClient();
   const payload: Prisma.OrderCreateInput = { ...data, tenantId };
 
   // Before Hooks
@@ -21,5 +22,6 @@ export const createdOrder = async (
 };
 
 export const getOrders = async (tenantId: string) => {
+  const prisma = getPrismaClient();
   return prisma.order.findMany({ where: { tenantId } });
 };
