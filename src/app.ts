f0d73cde4714c2aa8cba06affcb776ee38a6d74b
owusuvictorjnr <owusuvictor80@gmail.com>
2026-03-12 12:49:55 +0000

    fix: import hooks module in app.ts for event handling

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import { registerListeners } from "./events/register-listeners.js";
+import "./hooks/index.js";
 import { RegisterRouter } from "./modules/register-routes.js";
 
 export const createApp = () => {
